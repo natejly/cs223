7f2cf637db1854f5f0d97f0a794a412568d74d27
natejly <natejly@gmail.com>
2024-01-30 18:54:08 -0500

    outputs but not working

--- hw1/life.c
+++ hw1/life.c
@@ -40,7 +40,7 @@ int get_next_state(int field[ROWS][COLS], size_t i, size_t j)
 {
     int alive_neighbors = num_living_neighbors(field, i, j);
     bool alive = is_alive(field, i, j);
-    if(alive)
+    if (alive)
     {
         if (alive_neighbors == 2 || alive_neighbors == 3)
         {
@@ -64,7 +64,7 @@ void compute_next_gen(int cur_field[ROWS][COLS], int next_field[ROWS][COLS])
     {
         for (size_t j = 0; j < COLS; j++)
         {
-            next_field[i][j] = get_next_state(next_field, i, j);
+            next_field[i][j] = get_next_state(cur_field, i, j);
         }
     }
 }
--- hw1/main.c
+++ hw1/main.c
@@ -10,60 +10,41 @@ Author: Nate Ly
 #include "constants.h"
 #include "life.h"
 #include "helpers.h"
-
+void printArray(int array[ROWS][COLS])
+{
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            printf("%d\t", array[i][j]);
+        }
+        printf("\n");
+    }
+}
 int main(int argc, char *argv[])
 {
-    /*
-     * TODO: Accept two command-line arguments, which will appear in the
-     * following order:
-     * time_steps:
-     *      a positive integer describing how many "generations" the simulation
-     *      will run for.
-     * init:
-     *      a string consisting of the characters '0' and '1', of length ROWS * COLS,
-     *      representing the initial state for this run of Game of Life.
-     */
 
     int time_steps = atoi(argv[1]);
 
-    printf("Time Steps: %d\n", time_steps);
-    printf("Init String: %s\n", argv[2]);
-    /*
-     * TODO: Create a new statically-allocated array of size ROWS x COLS
-     */
-    char board [ROWS][COLS];
+    int board[ROWS][COLS];
     int init_index = 0;
     for (size_t i = 0; i < ROWS; i++)
     {
         for (size_t j = 0; j < COLS; j++)
         {
-            board[i][j] = argv[2][init_index];
-
+            board[i][j] = argv[2][init_index] - '0';
             init_index++;
         }
     }
-    /*
-     * TODO: Print the initial values held in the field
-     */
-    // for (int i = 0; i < ROWS; i++)
-    // {
-    //     for (int j = 0; j < COLS; j++)
-    //     {
-    //         printf("%c\t", board[i][j]);
-    //     }
-    //     printf("\n");
-    // }
+
     print_initial(board);
-    /*
-     * TODO: Run the game time_steps times, printing the field at the end of
-     * each step
-     */
-    for(size_t i = 0; i < time_steps; i++){
-        print_generation(board, i);
-    }
-    /*
-     * Print "Done." then exit.
-     */
+
+    for (size_t i = 0; i < time_steps; i++)
+    {
+        compute_next_gen(board, board);
+        print_generation(board, i + 1);
+        }
+
     printf("Done.\n");
     return 0;
 }
