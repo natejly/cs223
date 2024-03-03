5d32919ccb343c66db4fc68ff26b6b583f225e53
natejly <natejly@gmail.com>
2024-03-03 15:00:23 -0500

    started main

--- hw4/blotto.c
+++ hw4/blotto.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
-#include "gmap.h"
-int main(void) {
-  printf("Hello, world!\n");
-  return 0;
+# include <stdio.h>
+# include <stdlib.h>
+# include "entry.h"
+# include "gmap.h"
+# include "string_key.h"
+#define MAX_ID 31
+int main(int argc, char *argv[])
+{
+    if (argc < 2){
+        fprintf(stderr, "not enough args");
+        return 1;
+    }
+    int num_battles = argc - 1;
+    int *battlefields = malloc(num_battles * sizeof(int));
+    for (int i = 0; i < num_battles; i++){
+        battlefields[i] = atoi(argv[i+1]);
+    }
+
 }
