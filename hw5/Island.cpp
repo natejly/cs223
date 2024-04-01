2713f359400bfdc826991a58ac2ff220257f22ef
natejly <natejly@gmail.com>
2024-04-01 01:48:09 -0400

    island comments

--- hw5/Island.cpp
+++ hw5/Island.cpp
@@ -10,24 +10,24 @@
 
 #include "Island.h"
 #include <string>
-
+//basic island constructor
 Island::Island()
 {
     this->name = "NoName";
     this->access = false;
 }
-
+//island constructor with parameters
 Island::Island(string name, bool access)
 {
     this->name = name;
     this->access = access;
 }
-
+//checks if two islands are equal
 bool Island::isEqual(Island other)
 {
     return (this->name.compare(other.getName()) == 0) && (this->access == other.hasBerth());
 }
-
+//prints island
 void Island::print(ostream &outfile)
 {   
     char access;
