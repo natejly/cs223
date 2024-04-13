56e30332fe5ab53ec99d923fd322300461a960e1
Ly Nate <njl36@zoo.cs.yale.edu>
2024-04-13 16:21:32 -0400

    using swap colors

--- hw6-release/BSTNode.cpp
+++ hw6-release/BSTNode.cpp
@@ -879,27 +879,24 @@ BSTNode *BSTNode::rbt_eliminate_red_red_violation()
             case LR:
                 nb.g->mLeft = nb.g->mLeft->left_rotate();
                 nb.g = nb.g->right_rotate();
-                nb.g->mColor = BLACK;
-                nb.g->mRight->mColor = RED;
+                nb.g->swap_colors_with(nb.g->mRight);
                 nb.g->make_locally_consistent();
                 break;
             case LL:
                 nb.g = nb.g->right_rotate();
-                nb.g->mColor = BLACK;
-                nb.g->mRight->mColor = RED;
+                nb.g->swap_colors_with(nb.g->mRight);
+
                 nb.g->make_locally_consistent();
                 break;
             case RL:
                 nb.g->mRight = nb.g->mRight->right_rotate();
                 nb.g = nb.g->left_rotate();
-                nb.g->mColor = BLACK;
-                nb.g->mLeft->mColor = RED;
+                nb.g->swap_colors_with(nb.g->mLeft);
                 nb.g->make_locally_consistent();
                 break;
             case RR:
                 nb.g = nb.g->right_rotate();
-                nb.g->mColor = BLACK;
-                nb.g->mRight->mColor = RED;                
+                nb.g->swap_colors_with(nb.g->mLeft);
                 nb.g->make_locally_consistent();
                 break;
             default:
